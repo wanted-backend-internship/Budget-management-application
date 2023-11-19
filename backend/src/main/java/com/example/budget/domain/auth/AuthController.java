4396743ccbea78eb34753e6b5f42f4e7e9d742b7
sieunnnn <wldsmtldsm65@gmail.com>
2023-11-19 11:45:59 +0900

    [BE] Rename: 엔드 포인트 수정

--- backend/src/main/java/com/example/budget/domain/auth/AuthController.java
+++ backend/src/main/java/com/example/budget/domain/auth/AuthController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
 
-    @PostMapping(value = "/signup")
+    @PostMapping(value = "/register")
     public ResponseEntity<?> signup(@RequestBody SignupRequest signupRequest) {
         try {
             authService.signup(signupRequest);
